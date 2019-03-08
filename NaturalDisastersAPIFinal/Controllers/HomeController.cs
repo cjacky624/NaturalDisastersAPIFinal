eef92e0c8d8f8c7301b516e7654b18bee2cbf32f
Ronald Lemuel <Rlemuel1992@gmail.com>
2019-03-08 16:55:34 -0500

    Almost finished the loop to grab only US states

--- NaturalDisastersAPIFinal/Controllers/HomeController.cs
+++ NaturalDisastersAPIFinal/Controllers/HomeController.cs
@@ -12,7 +12,20 @@ namespace NaturalDisastersAPIFinal.Controllers
 {
     public class HomeController : Controller
     {
+		//Lists
 		public List<Earthquakes> EarthquakeList = new List<Earthquakes>();
+		//Lists
+		public List<string> USStates = new List<string>()
+		{
+			"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+			"Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky",
+			"Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi",
+			"Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico",
+			"New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania",
+			"Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont",
+			"Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"
+		};
+
 		public ActionResult Index()
         {
             return View();
@@ -21,7 +34,7 @@ namespace NaturalDisastersAPIFinal.Controllers
         public ActionResult About()
         {
 			string APIText = "https://earthquake.usgs.gov/fdsnws/" +
-				$"event/1/query?format=geojson&starttime=2000-03-04&&minmagnitude=6&";
+				$"event/1/query?format=geojson&starttime=1970-01-01&&minmagnitude=6&";
 
 			HttpWebRequest request = WebRequest.CreateHttp(APIText);
 			HttpWebResponse response = (HttpWebResponse)request.GetResponse();
@@ -33,12 +46,14 @@ namespace NaturalDisastersAPIFinal.Controllers
 			JToken ColorData = JToken.Parse(data);
 			List<JToken> TheColors = ColorData["features"].ToList();
 
-
-			for (int i = 0; i < 2936; i++)
+			for (int i = 0; i < TheColors.Count(); i++)
 			{
-				Earthquakes e = new Earthquakes();
-				e.Color = TheColors[i]["properties"]["alert"].ToString();
-				EarthquakeList.Add(e);
+				if (TheColors[i]["properties"]["place"].ToString().Contains(USStates.ToString()))
+				{
+					Earthquakes e = new Earthquakes();
+					e.Color = TheColors[i]["properties"]["alert"].ToString();
+					EarthquakeList.Add(e);
+				}
 
 			}
 			ViewBag.Results = EarthquakeList;
@@ -46,7 +61,16 @@ namespace NaturalDisastersAPIFinal.Controllers
 			return View();
         }
 
-        public ActionResult Contact()
+
+
+
+
+
+
+
+
+		
+public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
